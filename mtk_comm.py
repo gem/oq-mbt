943767ccafadf3bd1125f9ce7aed9b2310de24b2
Matteo Nastasi <nastasi@openquake.org>
2016-04-22 15:16:17 +0200

    shared configuration parameters module added

--- mtk_comm.py
+++ mtk_comm.py
@@ -12,3 +12,10 @@ g_message = None
 class Bunch(object):
     def __init__(self, **kw):
         vars(self).update(kw)
+
+
+def accordion_title_find(accord, name):
+    for i in range(0, len(accord.children)):
+        if accord.get_title(i) == name:
+            return i
+    return -1

a73e8a7aa693f10402d86037cf02e80f30ce50fd
Matteo Nastasi <nastasi@openquake.org>
2016-07-06 09:36:02 +0200

    better on-the-fly modules reset

--- cleaner.py
+++ cleaner.py
@@ -1,11 +1,28 @@
 import sys
-
+import os
 
 def modules_cleaner():
     global gem_cleaner
+
+    ve_path = os.getenv('VIRTUAL_ENV')
+
     if 'gem_cleaner' not in globals():
         gem_cleaner = sys.modules.copy()
     else:
         for k in list(sys.modules.keys()):
-            if k not in gem_cleaner:
+            if sys.modules[k] == None:
                 del sys.modules[k]
+                continue
+
+            if k not in gem_cleaner:
+                if k == 'pkg_resources':
+                    del sys.modules[k]
+                    continue
+                try:
+                    # print sys.modules[k].__file__
+                    if (sys.modules[k].__file__.startswith(ve_path) or
+                        sys.modules[k].__file__.startswith('/usr/lib/')):
+                        continue
+                    del sys.modules[k]
+                except:
+                    pass

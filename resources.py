5ce42e6b46f40014273016ece8a84decec7e0e3b
mpagani <marco.pagani@globalquakemodel.org>
2016-07-08 12:13:20 +0100

    Added folder containing tools e.g. importers

--- resources.py
+++ resources.py
@@ -39,6 +39,7 @@ mbt_importers = Importers_collection()
 # from oqmbt.china.china_tools import faults_to_hmtk
 mbt_importers.add(Importer('oqmbt.tools.china.area.areas_to_hmtk', 'china_areas_hmtk', 'China: from areas to hmtk'))
 mbt_importers.add(Importer('oqmbt.china.china_tools.faults_to_hmtk', 'china_faults_hmtk', 'China: from faults to hmtk'))
+mbt_importers.add(Importer('mbt.tools.imprt.faults', 'fmg_to_oqs', 'FMG: shallow faults'))
 
 class Resource_external_file(Dictable):
     __public__ = ["key", "filename", "loader", "onthefly", "checksum", "mtime" ]

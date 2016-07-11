2642dfaa2f1a0edf63081274016771a71b09e109
Matteo Nastasi <nastasi@openquake.org>
2016-07-11 15:24:24 +0200

    missing files added

---
+++ cells.py
@@ -0,0 +1,53 @@
+from serial import Dictable
+from IPython.display import display, Javascript
+
+
+def cells_cleanall():
+    display(Javascript("""
+      {
+          var cells = IPython.notebook.get_cells();
+          var ctx, ct = cells.length;
+          for (var i = 0 ; i < ct ; i++) {
+              if (i == 0) {
+                  cells[i].unselect();
+              }
+              else {
+                  cells[i].select();
+              }
+          }
+          IPython.notebook.delete_cell();
+      }
+    """))
+
+
+def cells_insert_at_bottom(type, content):
+    display(Javascript("""
+    {
+    var cell = IPython.notebook.insert_cell_at_bottom('%s');
+    cell.set_text('%s');
+    }
+    """ % (type,content)))
+
+            
+class Cell(Dictable):
+    __public__ = ["type", "content"]
+
+    def __init__(self, type, content):
+        self.type = type
+        self.content = content
+
+        
+class Cells(Dictable):
+    __public__ = ["cells"]
+
+    def __init__(self, cells=None):
+        if cells is None:
+            self.cells = []
+        else:
+            self.cells = cells
+
+
+    def load(self):
+        cells_cleanall()
+        for cell in self.cells:
+            cells_insert_at_bottom(cell.type, cell.content)
